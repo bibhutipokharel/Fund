1e5bda3c65e6ccc94a65e4e3b075185641b7f8f8
bibhutipokharel <126839898+bibhutipokharel@users.noreply.github.com>
2024-02-01 21:38:24 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('showQRButton').addEventListener('click', function() {
+    var qrContainer = document.getElementById('qrContainer');
+    qrContainer.style.display = qrContainer.style.display === 'none' ? 'block' : 'none';
+});
